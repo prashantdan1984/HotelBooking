f753464656284086ab9f0c0140eb576cbd38f440
Pradhantdan <gadhavi.prashantdan@gmail.com>
2024-02-06 18:06:58 +0530

    Initial Commit

---
+++ HotelBooking/HotelBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingApp.swift
+//  HotelBooking
+//
+//  Created by Prashantdan on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
